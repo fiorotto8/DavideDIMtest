e8792a9c82cf964e849a381bb0722283c61931d8
fiorotto8 <davide.fiorina01@universitadipavia.it>
2023-01-30 12:03:40 +0100

    Works on GASMON. t and perc are correcly gathered

--- dim_v20r33/src/examples/test_client.c
+++ dim_v20r33/src/examples/test_client.c
@@ -5,7 +5,7 @@
 
 //int t_alarm;
 typedef struct {
-	int t_alarm;
+	double t_alarm;
 	double percentage;
 }GM_var;
 
@@ -29,7 +29,7 @@ int *tag, *size;
 	printf("size = %d\n",*size);
 
 	memcpy(&gm, buf, (size_t)*size);
-	printf("Alarm : %d, Percentage: %f\n", gm.t_alarm, gm.percentage);
+	printf("Alarm : %f, Percentage: %f\n", gm.t_alarm, gm.percentage);
 	
 	dic_get_timestamp(0, &secs, &millis);
 	tsecs = secs;
@@ -71,140 +71,3 @@ int main(int argc, char **argv)
 	}
 	return 1;
 }
-
-/*
-char str[80];
-char str_res[10][80];
-char client_str[80];
-int no_link = -1;
-float no_link_float = -1.0;
-char buff[80];
-
-typedef struct {
-	int i;
-	int j;
-	int k;
-	double d;
-	short s;
-    char c;
-	short t;
-	float f;
-	char str[20];
-}TT;
-
-TT t;
-
-void big_rout( int *tag, int *buf, int *size )
-{
-
-	if(size){}
-	printf("Received %d for TestMem_%d\n", *buf, *tag);
-}
-
-
-void got_servers( int *tag, char *list, int *size)
-{
-	if(tag){}
-	if(size){}
-	printf("%s",list);
-}
-
-void got_services( int *tag, char *list, int *size)
-{
-	if(tag){}
-	if(size){}
-	printf("%s",list);
-}
-
-void version_rout( int *tag, int *buf, int *size)
-{
-	if(tag){}
-	printf("Received VERSION %lx, %d\n", (dim_long)buf, *size);
-}
-
-void rout( tag, buf, size )
-char *buf;
-int *tag, *size;
-{
-	if(*tag == 1100)
-	{
-		printf("Received ONCE_ONLY : %s\n",buff);
-		return;
-	}
-	if(*tag == 1200)
-	{
-		char node[128], str[256];
-		int secs, millis;
-		time_t tsecs;
-
-		dic_get_dns_node(node);
-		printf("DNS node = %s\n",node);
-		printf("size = %d\n",*size);
-		memcpy(&t, buf, (size_t)*size);
-		printf("t.i = %d, t.d = %2.2f, t.s = %d, t.c = %c, t.f = %2.2f, t.str = %s\n",
-			t.i,t.d,t.s,t.c,t.f,t.str);
-		dic_get_timestamp(0, &secs, &millis);
-		tsecs = secs;
-		my_ctime(&tsecs, str, 128);
-		str[(int)strlen(str)-1] = '\0';
-		printf("timestamp = %s.%d\n",str,millis);
-
-		return;
-	}
-	else
-		printf("%s Received %s for Service%03d\n",client_str,buf,*tag);
-
-}
-
-int main(int argc, char **argv)
-{
-	int i;
-	char aux[80];
-	int id = 123;
-
-	if(argc){}
-	//int dic_cmnd_service( char *serv_name, void *serv_address, int serv_size )
-	//non ho capito cosa faccia
-	sprintf(str,"%s/SET_EXIT_HANDLER",argv[2]);
-	dic_cmnd_service(str, &id, 4);
-	
-	//int dic_get_id(char *name)
-	dic_get_id(aux);
-	printf("ID: %s\n",aux);
-	//give name to client
-	strcpy(client_str,argv[1]);
-
-	for(i = 0; i< 10; i++)
-	{
-		sprintf(str,"%s/Service_%03d",argv[2],i);
-		dic_info_service( str, TIMED, 10, 0, 0, rout, i,
-			  "No Link", 8 );
-	}
-	
-	sprintf(aux,"%s/TEST_SWAP",argv[2]);
-	dic_info_service_stamped( aux, TIMED, 5, 0, 0, rout, 1200,
-			  &no_link, 4 );
-
-	sprintf(str,"%s/VERSION_NUMBER",argv[2]);
-	dic_info_service( str, MONITORED, 0, 0, 0, version_rout, 0,
-			  NULL, 0 );
-
-	sprintf(aux,"%s/TEST_CMD",argv[2]);
-
-	dim_long dnsid;
-	dnsid = dic_add_dns("lxplus613.cern.ch", 0);
-	for (i = 0; i< 10; i++)
-	{
-		sprintf(str, "%s1/Service_%03d", argv[2], i);
-		dic_info_service_dns(dnsid, str, TIMED, 10, 0, 0, rout, i,
-			"No Link", 8);
-	}
-	while(1)
-	{
-		sleep(1);
-		printf("Sending Command, size = %d, i = %d\n",(int)sizeof(t), t.i);
-		dic_cmnd_service(aux,&t,(int)sizeof(t));
-	}
-	return 1;
-}
-*/
--- dim_v20r33/src/examples/test_server.c
+++ dim_v20r33/src/examples/test_server.c
@@ -6,14 +6,50 @@
 //variables
 //int t_alarm=0;
 //double percentage=30.;
+//this file contain the running variables
+char file[] ="/home/gem3/GasMon/Readout/logs/gemreadout.log";
 
 typedef struct {
-	int t_alarm;
+	double t_alarm;
 	double percentage;
 }GM_var;
-
 GM_var gm;
 
+GM_var get_vars(char path[]) {
+	GM_var test_gm;
+
+	//test_gm.t_alarm=0;
+	//test_gm.percentage=0.;\
+
+	//read the file in path up to the end
+	//at the end of cycle the tmp variable will contain teh last row
+	char tmp[1024];
+	FILE *fp;
+	fp = fopen(path, "r");
+	while(!feof(fp))
+		fgets(tmp, 1024, fp);
+	//printf("%s", tmp);
+
+	//strip the last row with delimiter to get every variable
+	char *token = strtok(tmp, ",");
+	int i=0;
+	while( token != NULL ) {
+		token = strtok(NULL, ",");
+		//printf( "%d,  %s\n", i, token ); //printing each token
+		if(i==20){
+			//printf( "%d,  %s\n", i, token ); //printing each token
+			test_gm.percentage=atof(token);
+			//printf( "%d, %f,  %f\n",i, test_gm.t_alarm, test_gm.percentage );
+		};
+		if(i==23){
+			//printf( "%d,  %s\n", i, token ); //printing each token
+			test_gm.t_alarm=atof(token);
+			//printf( "%d, %f,  %f\n",i, test_gm.t_alarm, test_gm.percentage );
+			};
+		i++;
+	}
+	return test_gm; // Return the local structure
+} 
 
 void cmnd_rout(int *tag, GM_var *buf, int *size)
 {
@@ -21,7 +57,7 @@ void cmnd_rout(int *tag, GM_var *buf, int *size)
 	printf("#################################\n");
 	dim_print_date_time();
 	printf("\nCommand received, size = %d, GM_var size = %d:\n", *size, (int)sizeof(GM_var));
-	printf("buf->t_alarm = %d, buf->percentage = %f\n",
+	printf("buf->t_alarm = %f, buf->percentage = %f\n",
 			buf->t_alarm,buf->percentage);
 }
 
@@ -90,184 +126,17 @@ int main(int argc, char **argv)
 	
 	while(1)
 	{
-		gm.t_alarm++;
-		gm.percentage++;
+		//gm.t_alarm++;
+		//gm.percentage++;
+		gm=get_vars(file);
+
 		dis_update_service(serv_alarm);
+		printf("t_alarm = %f, percentage = %f\n", gm.t_alarm, gm.percentage);
+
+		
 		//printf("t_alarm = %d, percentage = %f\n", gm.t_alarm, gm.percentage);
-		sleep(5);
-	}
-	return 1;
-
-}
-
-
-
-
-
-
-/*
-char str[10][80];
-
-typedef struct {
-	int i;
-	int j;
-	int k;
-	double d;
-	short s;
-	char c;
-	short t;
-	float f;
-	char str[20];
-}TT;
-
-TT t;
-
-
-int big_buff[1024];
-
-int testDimInt = 0;
-int testDimLong = 0;
-longlong testDimXLong = 0;
-
-
-void cmnd_rout(int *tag, TT *buf, int *size)
-{
-	if(tag){}
-	dim_print_date_time();
-	printf("Command received, size = %d, TT size = %d:\n", *size,
-	       (int)sizeof(TT));
-	printf("buf->i = %d, buf->d = %2.2f, buf->s = %d, buf->c = %c, buf->f = %2.2f, buf->str = %s\n",
-			buf->i,buf->d,buf->s,buf->c,buf->f,buf->str);
-}
-
-void cmnd_rout_long(int *tag, longlong *buf, int *size)
-{
-
-	if (tag) {}
-	dim_print_date_time();
-	printf("Command received, size = %d\n", *size);
-	printf("contents = %lld, %llx\n", *buf, *buf);
-}
-
-void client_exited(int *tag)
-{
-	char name[84];
-
-	if(dis_get_client(name))
-		printf("Client %s (%d) exited\n", name, *tag);
-	else
-		printf("Client %d exited\n", *tag);
-}
-
-void exit_cmnd(int *code)
-{
-	printf("Exit_cmnd %d\n", *code);
-	exit(*code);
-}
-
-int NewData;
-int NewIds[11];
-
-int more_ids[1024];
-int curr_more_index = 0;
-char more_str[1024][80];
-
-int main(int argc, char **argv)
-{
-	int i, id, xlong_id, int_id;
-	char aux[80];
-	char name[84];
-	char srvname[64];
-
-	dim_long dnsid = 0;
-	char extra_dns[128];
-	int new_dns = 0;
-	int index = 0;
-
-	//rpint data and starting up
-	dim_print_date_time();
-	printf("Dim Server Starting up...\n");
-	fflush(stdout);
-	//this ad an extra DNS if needed (proably not needed)
-	if(argc){}
-	new_dns = dim_get_env_var("EXTRA_DNS_NODE", extra_dns, sizeof(extra_dns));
-	if(new_dns)
-		dnsid = dis_add_dns(extra_dns,0);
-	if(dnsid){}
-
-	//handeler for exit comand and exited status
-	dis_add_exit_handler(exit_cmnd);
-	dis_add_client_exit_handler(client_exited);
-
-	//setting server name
-	strcpy(srvname, argv[1]);
-
-	//ADD SERVICE METHOD
-	//dis_add_service( char *name, char *type, void *address, int size, 
-	//					 void (*user_routine)(), dim_long tag)
-
-	for(i = 0; i< 10; i++)
-	{
-		snprintf(str[i],sizeof(str[i]),"%s/Service_%03d",srvname,i);
-		dis_add_service( str[i], "C", str[i], (int)strlen(str[i])+1, 
-		(void *)0, 0 );
-	}
-	t.i = 123;
-	t.j = 456;
-	t.k = 789;
-	t.d = 56.78;
-	t.s = 12;
-	t.t = 12;
-	t.c = 'a';
-	t.f = (float)4.56;
-	strcpy(t.str,"hello world");
-
-	snprintf(aux, sizeof(aux),"%s/TEST_SWAP",srvname);
-	id = dis_add_service( aux, "l:3;d:1;s:1;c:1;s:1;f:1;c:20", &t, sizeof(t), 
-		(void *)0, 0 );
-
-	snprintf(aux, sizeof(aux),"%s/TEST_SWAP1",srvname);
-	id = dis_add_service( aux, "l:3;d:1;s:1;c:1;s:1;f:1;c:20", &t, sizeof(t), 
-		(void *)0, 0 );
-
-	//ADD COMMAND
-	//unsigned dis_add_cmnd( char *name, char *type, void (*user_routine)(), dim_long tag ) 
-	//the user_routine here is basically for prining out
-
-	if(id){}
-	snprintf(aux, sizeof(aux),"%s/TEST_CMD",srvname);
-	dis_add_cmnd(aux,"l:3;d:1;s:1;c:1;s:1;f:1;c:20",cmnd_rout, 0);
-
-	snprintf(aux, sizeof(aux), "%s/Service_DimInt", srvname);
-	int_id = dis_add_service(aux, "I", &testDimInt, sizeof(testDimInt),
-		(void *)0, 0);
-	snprintf(aux, sizeof(aux), "%s/Service_DimLong", srvname);
-	dis_add_service(aux, "L", &testDimLong, sizeof(testDimLong),
-		(void *)0, 0);
-	snprintf(aux, sizeof(aux), "%s/Service_DimXLong", srvname);
-	xlong_id = dis_add_service(aux, "X", &testDimXLong, sizeof(testDimXLong),
-		(void *)0, 0);
-
-	snprintf(aux, sizeof(aux), "%s/TEST_XCMD", srvname);
-	dis_add_cmnd(aux, "X", cmnd_rout_long, 0);
-	
-	dis_start_serving( argv[1] );
-
-	if(dis_get_client(name))
-	{
-		printf("client %s\n",name);
-	}
-
-	while(1)
-	{
-		index++;
-		testDimXLong++;
-		dis_update_service(xlong_id);
-		testDimInt = -index;
-		dis_update_service(int_id);
-
 		sleep(1);
 	}
 	return 1;
+
 }
-*/
